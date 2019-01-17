69db97d854b45caf5e9a1e755e9b9f3f2eb8bf9c
ardulogic <denncath@gmail.com>
2019-01-17 22:07:55 +0200

    Library Inception

---
+++ src/klynabirbis/Klynas.php
@@ -0,0 +1,10 @@
+<?php
+use Clarksonlib\Clarkson;
+
+namespace Klynabirbis;
+
+class Klynas {
+	public static function birbti() {
+		Clarkson::skaciuok_cal();
+	}
+}
---
+++ tests/klynabirbis/Klynas.php
@@ -0,0 +1,8 @@
+<?php
+namespace Klynabirbis;
+
+class Klynas {
+	function birbti() {
+		echo "Testuoju ar klynas birbia v1.0";
+	}
+}
